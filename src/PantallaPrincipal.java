cb8c7d53d7bfefb2b73d38da726cd42f81f96413
Antonio MS <yotoni7@gmail.com>
2015-03-02 17:27:09 +0100

    Actualización de label

--- src/PantallaPrincipal.java
+++ src/PantallaPrincipal.java
@@ -71,7 +71,7 @@ public final class PantallaPrincipal extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel2.setText("Peska cara huevo");
+        jLabel2.setText("joselu deja de esnifar semen");
 
         jButton1.setText("jButton1");
 
