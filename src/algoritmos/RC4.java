d4498621b09f6d5ea965704b721f9ed95ca2baf5
Antonio MS <yotoni7@gmail.com>
2015-03-03 01:13:24 +0100

    Implementación RC4

---
+++ src/algoritmos/RC4.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algoritmos;
+
+/**
+ *
+ * @author toni
+ */
+public class RC4 {
+ 
+    private char[] key;
+    private int[] sbox;
+    private static final int SBOX_LENGTH = 256;
+    private static final int KEY_MIN_LENGTH = 5;
+   
+    /*
+    public static void main(String[] args) {
+       try {
+            Scanner in = new Scanner(System.in);
+            System.out.print("Mensaje a cifrar: ");
+            String text = in.nextLine().toLowerCase();
+            System.out.print("Clave (min 5 dígitos): ");
+            String testKey = in.nextLine().toLowerCase();  
+            RC4 rc4 = new RC4(testKey);
+            char[] result = rc4.encrypt(text.toCharArray());
+            System.out.println("Mensaje cifrado RC4:\n" + new String(result));
+            System.out.println("Mensaje descifrado RC4:\n"
+                    + new String(rc4.decrypt(result)));
+        } catch (InvalidKeyException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+    */
+    public RC4(String key) throws InvalidKeyException {
+        setKey(key);
+    }
+ 
+    public RC4() {
+    }
+ 
+    public char[] decrypt(final char[] msg) {
+        return encrypt(msg);
+    }
+ 
+    public char[] encrypt(final char[] msg) {
+        sbox = initSBox(key);
+        char[] code = new char[msg.length];
+        int i = 0;
+        int j = 0;
+        for (int n = 0; n < msg.length; n++) {
+            i = (i + 1) % SBOX_LENGTH;
+            j = (j + sbox[i]) % SBOX_LENGTH;
+            swap(i, j, sbox);
+            int rand = sbox[(sbox[i] + sbox[j]) % SBOX_LENGTH];
+            code[n] = (char) (rand ^ (int) msg[n]);
+        }
+        return code;
+    }
+ 
+    private int[] initSBox(char[] key) {
+        int[] sbox = new int[SBOX_LENGTH];
+        int j = 0;
+ 
+        for (int i = 0; i < SBOX_LENGTH; i++) {
+            sbox[i] = i;
+        }
+ 
+        for (int i = 0; i < SBOX_LENGTH; i++) {
+            j = (j + sbox[i] + key[i % key.length]) % SBOX_LENGTH;
+            swap(i, j, sbox);
+        }
+        return sbox;
+    }
+ 
+    private void swap(int i, int j, int[] sbox) {
+        int temp = sbox[i];
+        sbox[i] = sbox[j];
+        sbox[j] = temp;
+    }
+ 
+    public void setKey(String key) throws InvalidKeyException {
+        if (!(key.length() >= KEY_MIN_LENGTH && key.length() < SBOX_LENGTH)) {
+            throw new InvalidKeyException("La clave tiene que tener una longitud entre  "
+                    + KEY_MIN_LENGTH + " y " + (SBOX_LENGTH - 1));
+        }
+ 
+        this.key = key.toCharArray();
+    }
+    public class InvalidKeyException extends Exception {
+ 
+        private static final long serialVersionUID = 1L;
+ 
+         public InvalidKeyException(String message) {
+            super(message);
+        }
+    } 
+}
